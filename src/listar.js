e72d3eef7de497fd533e41fbb8463d5a32d27dd7
goncadanilo <gonca.danilo@gmail.com>
2019-08-20 22:33:34 -0300

    Update listar.js

--- src/listar.js
+++ src/listar.js
@@ -3,21 +3,11 @@ if (localStorage.length > 0) {
     const user = localStorage.getItem(i + 1).split(";");
     document.getElementById("list").innerHTML +=
       "<tr>" +
-      "<td>" +
-      user[0] +
-      "</td>" +
-      "<td>" +
-      user[1] +
-      "</td>" +
-      "<td>" +
-      user[2] +
-      "</td>" +
-      "<td>" +
-      user[3] +
-      "</td>" +
-      "<td>" +
-      user[4] +
-      "</td>" +
+        "<td>" + user[0] + "</td>" +
+        "<td>" + user[1] + "</td>" +
+        "<td>" + user[2] + "</td>" +
+        "<td>" + user[3] + "</td>" +
+        "<td>" + user[4] + "</td>" +
       "</tr>";
   }
 }
